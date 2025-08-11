992eb5e98c0e826e7f285d53d35c52990d537cb5
sne <igordnepr27@gmail.com>
2025-08-11 14:11:50 +0200

    виніс обробку broadcast в окремі функції

--- p2p/broadcast_handler.go
+++ p2p/broadcast_handler.go
@@ -11,6 +11,8 @@ import (
 
 // Читання вхідних повідомлень
 func (n *Node) handleBroadcastMessages() {
+	// NOTE: думаю, що треба зробити функцію для кожного випадка і обробляти її типу: go handleMsgNewTransaction(), тому що коли буде багато вхідних повідомлень, воно може їх просто пропускати
+	// або винести усю обробку в окрему gorutine, а n.topic.sub.Next буде кидати усі нові повідомлення в канал. таким чином можна зберегти послідовну обробку і не пропоскати повідомлення
 	for {
 		msg, err := n.topic.sub.Next(n.ctx)
 		if err != nil {
@@ -38,105 +40,110 @@ func (n *Node) handleBroadcastMessages() {
 			continue
 		}
 
-		switch message.Type { // NOTE: думаю, що треба зробити функцію для кожного випадка і обробляти її типу: go handleMsgNewTransaction(), тому що коли буде багато вхідних повідомлень, воно може їх просто пропускати
+		switch message.Type {
 		case MsgNewTransaction:
-			var tx chain.Transaction
-			err = json.Unmarshal(message.Data, &tx)
-			if err != nil {
-				log.Error().Err(err).Msg("помилка розпаковки транзакції")
-				continue
-			}
-
-			log.Info().Int64("latency", time.Now().UnixMilli()-tx.Timestamp).Msg("отримано транзакцію")
-
-			if err = n.mempool.Add(&tx); err != nil {
-				log.Warn().Err(err).Msg("отрмана транзакція не була додана до mempool")
-			}
-
+			go n.handleMsgNewTransaction(message.Data)
 		case MsgBlockProposal:
-			var block chain.Block
-			err = json.Unmarshal(message.Data, &block)
-			if err != nil {
-				log.Error().Err(err).Msg("помилка розпаковки blockProposal")
-				continue
+			go n.handleMsgBlockProposal(message.Data)
+		}
+	}
+}
+
+func (n *Node) handleMsgNewTransaction(data []byte) {
+	var tx chain.Transaction
+	err := json.Unmarshal(data, &tx)
+	if err != nil {
+		log.Error().Err(err).Msg("помилка розпаковки транзакції")
+		return
+	}
+
+	log.Info().Int64("latency", time.Now().UnixMilli()-tx.Timestamp).Msg("отримано транзакцію")
+
+	if err = n.mempool.Add(&tx); err != nil {
+		log.Warn().Err(err).Msg("отрмана транзакція не була додана до mempool")
+	}
+}
+
+func (n *Node) handleMsgBlockProposal(data []byte) {
+	var block chain.Block
+	err := json.Unmarshal(data, &block)
+	if err != nil {
+		log.Error().Err(err).Msg("помилка розпаковки blockProposal")
+		return
+	}
+	log.Info().Uint32("height", block.Height).Int64("latency", time.Now().UnixMilli()-block.Timestamp).Msg("отримано новий блок")
+
+	// NOTE: я ще не впевнений в MsgVote, тому що, якщо я перевірив блок, і він правельний, то це означає, що усі за нього проголосують
+	// TODO: додати перевірку автора ( щоб pubkey збігався з тим, хто повинен був робити блок ). І нагороду, яку він собі назначив
+	lastLocalBlock, err := n.bs.GetLastBlock()
+	if err != nil {
+		log.Fatal().Err(err).Msg("помилка отримання останнього блоку")
+	}
+
+	// Перевірка блоку
+	if err = block.Verify(); err != nil {
+		log.Err(err).Msg("помилка перевірки блоку")
+		return
+	}
+	if err = block.VerifyTransactions(); err != nil {
+		log.Err(err).Msg("транзакції блоку не є валідними")
+		return
+	}
+	if lastLocalBlock.Height+1 != block.Height {
+		log.Error().Uint32("висота отриманого блоку: ", block.Height).Uint32("очікувана висота", lastLocalBlock.Height+1).Msg("помилка висоти блоку")
+		return
+	}
+
+	n.bs.SaveBlock(&block)
+
+	// видалити транзакції з mempool, якщо вони є в блоці
+	go func() {
+		for _, tx := range block.Transactions {
+			n.mempool.DeleteIfExist(tx)
+		}
+	}()
+
+	for _, tx := range block.Transactions {
+		if bytes.Equal(tx.To, []byte(STAKE)) {
+			validator := chain.Validator{
+				Address: tx.PubKey, // NOTE: досі не вирішив, чи я використовую hash або pub
+				Amount:  tx.Amount,
 			}
-			log.Info().Uint32("height", block.Height).Int64("latency", time.Now().UnixMilli()-block.Timestamp).Msg("отримано новий блок")
 
-			// NOTE: я ще не впевнений в MsgVote, тому що, якщо я перевірив блок, і він правельний, то це означає, що усі за нього проголосують
-			// TODO: додати перевірку автора ( щоб pubkey збігався з тим, хто повинен був робити блок ). І нагороду, яку він собі назначив
-			lastLocalBlock, err := n.bs.GetLastBlock()
-			if err != nil {
-				log.Fatal().Err(err).Msg("помилка отримання останнього блоку")
-			}
+			n.bs.AddValidator(&validator)
+		}
+	}
 
-			// Перевірка блоку
-			if err = block.Verify(); err != nil {
-				log.Err(err).Msg("помилка перевірки блоку")
-				continue
-			}
-			if err = block.VerifyTransactions(); err != nil {
-				log.Err(err).Msg("транзакції блоку не є валідними")
-				continue
-			}
-			if lastLocalBlock.Height+1 != block.Height {
-				log.Error().Uint32("висота отриманого блоку: ", block.Height).Uint32("очікувана висота", lastLocalBlock.Height+1).Msg("помилка висоти блоку")
-				continue
-			}
+	val, err := n.chooseValidator()
+	if err != nil {
+		log.Error().Err(err).Msg("помилка вибору наступного валідатора")
+		return
+	}
 
-			n.bs.SaveBlock(&block)
+	// я це і є настпуний валідатор!
+	if bytes.Equal(val.Address, n.keys.Pub) {
+		newBlock := n.createNewBlock()
 
-			// видалити транзакції з mempool, якщо вони є в блоці
-			go func() {
-				for _, tx := range block.Transactions {
-					n.mempool.DeleteIfExist(tx)
-				}
-			}()
+		newBlockBytes, err := json.Marshal(newBlock)
+		if err != nil {
+			log.Fatal().Err(err).Msg("помилка розпаковки нового блоку")
+		}
 
-			for _, tx := range block.Transactions {
-				if bytes.Equal(tx.To, []byte(STAKE)) {
-					validator := chain.Validator{
-						Address: tx.PubKey, // NOTE: досі не вирішив, чи я використовую hash або pub
-						Amount:  tx.Amount,
-					}
+		blockProposalMsg := Message{
+			Type:      MsgBlockProposal,
+			Timestamp: time.Now().UnixMilli(),
+			Data:      newBlockBytes,
+			Pub:       n.keys.Pub,
+		}
 
-					n.bs.AddValidator(&validator)
-				}
-			}
+		err = blockProposalMsg.sign(n.keys.Priv)
+		if err != nil {
+			log.Fatal().Err(err).Msg("помилка підпису нового блоку")
+		}
 
-			val, err := n.chooseValidator()
-			if err != nil {
-				log.Error().Err(err).Msg("помилка вибору наступного валідатора")
-				continue
-			}
-
-			// я це і є настпуний валідатор!
-			if bytes.Equal(val.Address, n.keys.Pub) {
-				go func() {
-					newBlock := n.createNewBlock()
-
-					newBlockBytes, err := json.Marshal(newBlock)
-					if err != nil {
-						log.Fatal().Err(err).Msg("помилка розпаковки нового блоку")
-					}
-
-					blockProposalMsg := Message{
-						Type:      MsgBlockProposal,
-						Timestamp: time.Now().UnixMilli(),
-						Data:      newBlockBytes,
-						Pub:       n.keys.Pub,
-					}
-
-					err = blockProposalMsg.sign(n.keys.Priv)
-					if err != nil {
-						log.Fatal().Err(err).Msg("помилка підпису нового блоку")
-					}
-
-					err = n.topic.broadcast(&blockProposalMsg, n.ctx)
-					if err != nil {
-						log.Error().Err(err).Msg("помилка трансляції нового блоку")
-					}
-				}()
-			}
+		err = n.topic.broadcast(&blockProposalMsg, n.ctx)
+		if err != nil {
+			log.Error().Err(err).Msg("помилка трансляції нового блоку")
 		}
 	}
 }

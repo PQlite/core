dad326e2d30596f670d80a647373b842e74373df
sne <igordnepr27@gmail.com>
2025-08-12 02:01:08 +0200

    видалив BlockForSign і адаптував до змін в crypto

--- chain/block.go
+++ chain/block.go
@@ -7,7 +7,6 @@ import (
 	"bytes"
 	"crypto/sha3"
 	"encoding/json"
-	"fmt"
 	"sort"
 
 	"github.com/PQlite/crypto"
@@ -20,53 +19,44 @@ type Block struct {
 	PrevHash     []byte         // Хеш попереднього блоку
 	Hash         []byte         // Хеш цього блоку (розраховується по іншим полям)
 	Proposer     []byte         // Адреса або публічний ключ того, хто створив блок
-	Signature    []byte         // Підпис Proposer'а на блоку
 	Transactions []*Transaction // Список транзакцій
-}
-
-type BlockForSign struct {
-	Height       uint32
-	Timestamp    int64
-	PrevHash     []byte
-	Proposer     []byte
-	Transactions []*Transaction
+	Signature    []byte         // Підпис Proposer'а на блоку
 }
 
 // sortTransactions сортує транзакції в блоці за їх підписами.
 // Це необхідно для детерміністичної серіалізації.
-func (b *BlockForSign) sortTransactions() {
+func (b *Block) sortTransactions() {
 	sort.Slice(b.Transactions, func(i, j int) bool {
 		return bytes.Compare(b.Transactions[i].Signature, b.Transactions[j].Signature) < 0
 	})
 }
 
-func (b *BlockForSign) Sign(binPriv []byte) (Block, error) {
+func (b *Block) Sign(binPriv []byte) error {
 	b.sortTransactions()
 
 	BlockForSignBytes, err := json.Marshal(*b)
 	if err != nil {
-		return Block{}, err
+		return err
 	}
 	sig, err := crypto.Sign(binPriv, BlockForSignBytes)
 	if err != nil {
-		return Block{}, err
+		return err
 	}
 
+	// TODO: винести в окрему функцію
+	// NOTE: створюєтся без signature, тому в ньому просто не має сенсу
 	blockHash := sha3.Sum224(BlockForSignBytes)
 
-	return Block{
-		Height:       b.Height,
-		Timestamp:    b.Timestamp,
-		PrevHash:     b.PrevHash,
-		Hash:         blockHash[:],
-		Proposer:     b.Proposer,
-		Signature:    sig,
-		Transactions: b.Transactions,
-	}, nil
+	b.Hash = blockHash[:]
+	b.Signature = sig
+
+	return nil
 }
 
 func (b *Block) Verify() error {
-	blockForVerify := BlockForSign{
+	// NOTE: можна зробити і краще
+	//       скопіювати b і видалити hash, signature
+	blockForVerify := Block{
 		Height:       b.Height,
 		Timestamp:    b.Timestamp,
 		PrevHash:     b.PrevHash,
@@ -82,14 +72,11 @@ func (b *Block) Verify() error {
 		return err
 	}
 
-	res, err := crypto.Verify(b.Proposer, binBlockForVerify, b.Signature)
-	if err != nil {
+	if err = crypto.Verify(b.Proposer, binBlockForVerify, b.Signature); err != nil {
 		log.Error().Err(err).Msg("помилка перевірки підпису блоку")
 		return err
 	}
-	if !res {
-		return fmt.Errorf("invalid block signature")
-	}
+
 	return nil
 }
 
@@ -103,4 +90,15 @@ func (b *Block) VerifyTransactions() error {
 		}
 	}
 	return nil
+}
+
+func (b Block) MarshalDeterministic() ([]byte, error) {
+	b.sortTransactions()
+
+	res, err := json.Marshal(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
